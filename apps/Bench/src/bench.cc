f01d5c3c97495d82c8c0457a26a31e5326d564e0
Danny McGee <dannymcgee@gmail.com>
2023-04-02 17:58:27 -0400

    Update benchmarks

--- apps/Bench/src/bench.cc
+++ apps/Bench/src/bench.cc
@@ -1,5 +1,6 @@
 #include <benchmark/benchmark.h>
 
+#include <math/matrix.h>
 #include <math/vector.h>
 #include <sized.h>
 
@@ -7,21 +8,20 @@ using namespace sized; // NOLINT
 using benchmark::State;
 using benchmark::DoNotOptimize;
 
-using Vec3f = math::Vector<3, f32>;
-using Vec3 = math::Vector<3, f64>;
+using math::Matrix;
+using Vec3f = math::Vector<3,f32>;
+using Vec3 = math::Vector<3,f64>;
 
 // Constructor (sanity check)
 static void BM_Ctor_f32(State& state)
 {
 	for (auto _ : state)
-		for (usize i = 0; i < 1000; ++i)
-			DoNotOptimize(Vec3f{ 1, 2, 3 });
+		DoNotOptimize(Vec3f{ 1, 2, 3 });
 }
 static void BM_Ctor_f64(State& state)
 {
 	for (auto _ : state)
-		for (usize i = 0; i < 1000; ++i)
-			DoNotOptimize(Vec3{ 1, 2, 3 });
+		DoNotOptimize(Vec3{ 1, 2, 3 });
 }
 BENCHMARK(BM_Ctor_f32); // NOLINT
 BENCHMARK(BM_Ctor_f64); // NOLINT
@@ -30,23 +30,19 @@ BENCHMARK(BM_Ctor_f64); // NOLINT
 // Cross-Product
 static void BM_CrossProduct_f32(State& state)
 {
-	for (auto _ : state) {
-		for (usize i = 0; i < 1000; ++i) {
-			auto a = Vec3f{ 1.f, 3.f, 4.f };
-			auto b = Vec3f{ 2.f, -5.f, 8.f };
-			DoNotOptimize(a.cross(b));
-		}
-	}
+	auto a = Vec3f{ 1.f, 3.f, 4.f };
+	auto b = Vec3f{ 2.f, -5.f, 8.f };
+
+	for (auto _ : state)
+		DoNotOptimize(a.cross(b));
 }
 static void BM_CrossProduct_f64(State& state)
 {
-	for (auto _ : state) {
-		for (usize i = 0; i < 1000; ++i) {
-			auto a = Vec3{ 1.0, 3.0, 4.0 };
-			auto b = Vec3{ 2.0, -5.0, 8.0 };
-			DoNotOptimize(a.cross(b));
-		}
-	}
+	auto a = Vec3{ 1.0, 3.0, 4.0 };
+	auto b = Vec3{ 2.0, -5.0, 8.0 };
+
+	for (auto _ : state)
+		DoNotOptimize(a.cross(b));
 }
 BENCHMARK(BM_CrossProduct_f32); // NOLINT
 BENCHMARK(BM_CrossProduct_f64); // NOLINT
@@ -55,21 +51,15 @@ BENCHMARK(BM_CrossProduct_f64); // NOLINT
 // Length
 static void BM_Length_f32(State& state)
 {
-	for (auto _ : state) {
-		for (usize i = 0; i < 1000; ++i) {
-			auto vec = Vec3f{ 1.f, 2.f, 3.f };
-			DoNotOptimize(vec.length());
-		}
-	}
+	auto vec = Vec3f{ 1.f, 2.f, 3.f };
+	for (auto _ : state)
+		DoNotOptimize(vec.length());
 }
 static void BM_Length_f64(State& state)
 {
-	for (auto _ : state) {
-		for (usize i = 0; i < 1000; ++i) {
-			auto vec = Vec3{ 1.0, 2.0, 3.0 };
-			DoNotOptimize(vec.length());
-		}
-	}
+	auto vec = Vec3{ 1.0, 2.0, 3.0 };
+	for (auto _ : state)
+		DoNotOptimize(vec.length());
 }
 BENCHMARK(BM_Length_f32); // NOLINT
 BENCHMARK(BM_Length_f64); // NOLINT
@@ -78,21 +68,15 @@ BENCHMARK(BM_Length_f64); // NOLINT
 // Normal
 static void BM_Normal_f32(State& state)
 {
-	for (auto _ : state) {
-		for (usize i = 0; i < 1000; ++i) {
-			auto vec = Vec3f{ 1.f, 2.f, 3.f };
-			DoNotOptimize(vec.normal());
-		}
-	}
+	auto vec = Vec3f{ 1.f, 2.f, 3.f };
+	for (auto _ : state)
+		DoNotOptimize(vec.normal());
 }
 static void BM_Normal_f64(State& state)
 {
-	for (auto _ : state) {
-		for (usize i = 0; i < 1000; ++i) {
-			auto vec = Vec3{ 1.0, 2.0, 3.0 };
-			DoNotOptimize(vec.normal());
-		}
-	}
+	auto vec = Vec3{ 1.0, 2.0, 3.0 };
+	for (auto _ : state)
+		DoNotOptimize(vec.normal());
 }
 BENCHMARK(BM_Normal_f32); // NOLINT
 BENCHMARK(BM_Normal_f64); // NOLINT
@@ -101,21 +85,15 @@ BENCHMARK(BM_Normal_f64); // NOLINT
 // Length and Direction
 static void BM_LengthAndDirection_f32(State& state)
 {
-	for (auto _ : state) {
-		for (usize i = 0; i < 1000; ++i) {
-			auto vec = Vec3f{ 1.f, 2.f, 3.f };
-			DoNotOptimize(vec.length_and_direction());
-		}
-	}
+	auto vec = Vec3f{ 1.f, 2.f, 3.f };
+	for (auto _ : state)
+		DoNotOptimize(vec.length_and_direction());
 }
 static void BM_LengthAndDirection_f64(State& state)
 {
-	for (auto _ : state) {
-		for (usize i = 0; i < 1000; ++i) {
-			auto vec = Vec3f{ 1.0, 2.0, 3.0 };
-			DoNotOptimize(vec.length_and_direction());
-		}
-	}
+	auto vec = Vec3f{ 1.0, 2.0, 3.0 };
+	for (auto _ : state)
+		DoNotOptimize(vec.length_and_direction());
 }
 BENCHMARK(BM_LengthAndDirection_f32); // NOLINT
 BENCHMARK(BM_LengthAndDirection_f64); // NOLINT
@@ -124,23 +102,19 @@ BENCHMARK(BM_LengthAndDirection_f64); // NOLINT
 // Distance
 static void BM_Distance_f32(State& state)
 {
-	for (auto _ : state) {
-		for (usize i = 0; i < 1000; ++i) {
-			auto a = Vec3f{ 1.f, 3.f, 4.f };
-			auto b = Vec3f{ 2.f, -5.f, 8.f };
-			DoNotOptimize(Vec3f::dist(a, b));
-		}
-	}
+	auto a = Vec3f{ 1.f, 3.f, 4.f };
+	auto b = Vec3f{ 2.f, -5.f, 8.f };
+
+	for (auto _ : state)
+		DoNotOptimize(Vec3f::dist(a, b));
 }
 static void BM_Distance_f64(State& state)
 {
-	for (auto _ : state) {
-		for (usize i = 0; i < 1000; ++i) {
-			auto a = Vec3{ 1.0, 3.0, 4.0 };
-			auto b = Vec3{ 2.0, -5.0, 8.0 };
-			DoNotOptimize(Vec3::dist(a, b));
-		}
-	}
+	auto a = Vec3{ 1.0, 3.0, 4.0 };
+	auto b = Vec3{ 2.0, -5.0, 8.0 };
+
+	for (auto _ : state)
+		DoNotOptimize(Vec3::dist(a, b));
 }
 BENCHMARK(BM_Distance_f32); // NOLINT
 BENCHMARK(BM_Distance_f64); // NOLINT
@@ -149,26 +123,36 @@ BENCHMARK(BM_Distance_f64); // NOLINT
 // Dot-Product
 static void BM_DotProduct_f32(State& state)
 {
-	for (auto _ : state) {
-		for (usize i = 0; i < 1000; ++i) {
-			auto a = Vec3f{ 1.f, 3.f, 4.f };
-			auto b = Vec3f{ 2.f, -5.f, 8.f };
-			DoNotOptimize(a.dot(b));
-		}
-	}
+	auto a = Vec3f{ 1.f, 3.f, 4.f };
+	auto b = Vec3f{ 2.f, -5.f, 8.f };
+
+	for (auto _ : state)
+		DoNotOptimize(a.dot(b));
 }
 static void BM_DotProduct_f64(State& state)
 {
-	for (auto _ : state) {
-		for (usize i = 0; i < 1000; ++i) {
-			auto a = Vec3{ 1.0, 3.0, 4.0 };
-			auto b = Vec3{ 2.0, -5.0, 8.0 };
-			DoNotOptimize(a.dot(b));
-		}
-	}
+	auto a = Vec3{ 1.0, 3.0, 4.0 };
+	auto b = Vec3{ 2.0, -5.0, 8.0 };
+
+	for (auto _ : state)
+		DoNotOptimize(a.dot(b));
 }
 BENCHMARK(BM_DotProduct_f32); // NOLINT
 BENCHMARK(BM_DotProduct_f64); // NOLINT
+
+
+static void BM_MatrixDeterminant(State& state)
+{
+	auto mat = Matrix<3,3,f64>{
+		{ -4.0, -3.0,  3.0 },
+		{  0.0,  2.0, -2.0 },
+		{  1.0,  4.0, -1.0 },
+	};
+
+	for (auto _ : state)
+		DoNotOptimize(mat.determinant());
+}
+BENCHMARK(BM_MatrixDeterminant); // NOLINT
 
 
 BENCHMARK_MAIN(); // NOLINT
