408baeca40e5ef32c4926e393b2e4ceb1eb00d8f
Danny McGee <dannymcgee@gmail.com>
2023-04-24 01:21:15 -0400

    Hello Triangle

--- apps/Renderer/src/main.cc
+++ apps/Renderer/src/main.cc
@@ -7,7 +7,7 @@ auto main() -> int
 		return -1;
 
 	// Create a windowed mode window and its OpenGL context
-	auto* window = glfwCreateWindow(1920, 1080, "Hello OpenGL", nullptr, nullptr);
+	auto* window = glfwCreateWindow(1280, 960, "Hello Triangle", nullptr, nullptr);
 	if (!window) {
 		glfwTerminate();
 
@@ -19,6 +19,13 @@ auto main() -> int
 	while (!glfwWindowShouldClose(window)) {
 		// Render here
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+		glVertex2f( -0.5, -0.5 );
+		glVertex2f(  0.0,  0.5 );
+		glVertex2f(  0.5, -0.5 );
+		glEnd();
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
